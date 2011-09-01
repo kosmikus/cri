73ff694dc025da5ceeee8e6ce640054fd7de236b
Andres Loeh <mail@andres-loeh.de>
2011-09-01 12:18:46 +0200

    Renaming Generates to Source.

---
+++ src/Random/CRI/Source.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE TypeFamilies, KindSignatures, MultiParamTypeClasses, FlexibleContexts #-}
+module Random.CRI.Source where
+
+import Control.Monad.Primitive
+
+class Monad m => Source m a where
+  random   :: m a
+
+class Monad m => RangedSource m a where
+  randomR  :: (a, a) -> m a
+
+class PrimMonad m => PrimSource m g a where
+  prandom  :: g m -> m a
+
+class PrimMonad m => PrimRangedSource m g a where
+  prandomR :: (a, a) -> g m -> m a
