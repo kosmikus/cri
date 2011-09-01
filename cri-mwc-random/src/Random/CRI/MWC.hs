b3f8a355e119f8378ca21d0d204ffc5e19bb85db
Andres Loeh <mail@andres-loeh.de>
2011-09-01 16:40:40 +0200

    Adding initializers to cri-mwc-random.

--- cri-mwc-random/src/Random/CRI/MWC.hs
+++ cri-mwc-random/src/Random/CRI/MWC.hs
@@ -1,22 +1,31 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, FlexibleContexts #-}
 module Random.CRI.MWC where
 
 import Control.Monad.Primitive
 import Control.Monad.Reader
+import Data.Vector.Generic
+import Data.Word
 import Random.CRI
 import Random.CRI.Utils
-import System.Random.MWC
+import qualified System.Random.MWC as R
 
-newtype MWC m = MWC { unMWC :: Gen (PrimState m) }
+newtype MWC m = MWC { unMWC :: R.Gen (PrimState m) }
 
-instance (PrimMonad m, Variate a) => PrimSource       m MWC a where
-  prandom    g = uniform    (unMWC g)
+instance (PrimMonad m, R.Variate a) => PrimSource       m MWC a where
+  prandom    g = R.uniform    (unMWC g)
 
-instance (PrimMonad m, Variate a) => PrimRangedSource m MWC a where
-  prandomR r g = uniformR r (unMWC g)
+instance (PrimMonad m, R.Variate a) => PrimRangedSource m MWC a where
+  prandomR r g = R.uniformR r (unMWC g)
 
-instance (PrimMonad m, Variate a) => Source       (ReaderT (MWC m) m) a where
+instance (PrimMonad m, R.Variate a) => Source       (ReaderT (MWC m) m) a where
   random    = readerWrap (lift . prandom   )
 
-instance (PrimMonad m, Variate a) => RangedSource (ReaderT (MWC m) m) a where
+instance (PrimMonad m, R.Variate a) => RangedSource (ReaderT (MWC m) m) a where
   randomR r = readerWrap (lift . prandomR r)
+
+create :: (PrimMonad m) => m (MWC m)
+create = liftM MWC R.create
+
+initialize :: (PrimMonad m, Vector v Word32) => v Word32 -> m (MWC m)
+initialize = liftM MWC . R.initialize
+
